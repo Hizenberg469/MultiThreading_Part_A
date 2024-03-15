48cc4b011e58c12aa87f7b5757f379d269c8c462
Hizenberg <growlingraptor@gmail.com>
2024-03-15 17:56:04 +0530

    assignment 5

---
+++ ThreadCancellation_async/Listener_Thread/udp_sender/udp_sender.c
@@ -0,0 +1,9 @@
+﻿#include "network_util.h"
+
+int
+main(int argc, char** argv) {
+
+	printf("Dest = [%s,%d] \n", argv[1], atoi(argv[2]));
+	send_udp_msg(argv[1], atoi(argv[2]), argv[3], strlen(argv[3]));
+	return 0;
+}
